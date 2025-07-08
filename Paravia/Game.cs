c383e00d10760e2abfd77841d104f83ce700be11
Daniel Gilbert <ich@danielgilbert.de>
2025-07-09 00:29:39 +0200

    Implements ReleaseGrain

--- Paravia/Game.cs
+++ Paravia/Game.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32.SafeHandles;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -46,6 +47,14 @@ namespace Paravia
         public int Rats { get; internal set; }
         public int GrainDemand { get; internal set; }
         public int RatsAte { get; internal set; }
+        public int SoldierPay { get; internal set; }
+        public int MarketRevenue { get; internal set; }
+        public int NewSerfs { get; internal set; }
+        public int DeadSerfs { get; internal set; }
+        public int TransplantedSerfs { get; internal set; }
+        public int FleeingSerfs { get; internal set; }
+        public int JusticeRevenue { get; internal set; }
+        public int MillRevenue { get; internal set; }
     }
 
     public class Game
@@ -197,7 +206,7 @@ namespace Paravia
                     continue;
                 }
 
-                Console.WriteLine("Is {0} male or female (m or f)?");
+                Console.WriteLine("Is {0} male or female (m or f)?", Players[i].Name);
                 string? gender = Console.ReadLine()?.ToLower() ?? "f";
                 bool isMale = (gender[0] == 'm');
                 Players.Add(new Player());
@@ -272,30 +281,394 @@ namespace Paravia
                     {
                         if (players[i].Soldiers > (player.Soldiers * 2.4))
                         {
-                            AttackNeighbor(players[i], player);
+                            //AttackNeighbor(players[i], player);
                             i = 9;
                         }
                     }
                 }
 
                 if (i != 9)
-                    AttackNeighbor(baron, player);
+                {
+                    //AttackNeighbor(baron, player);
+                }
+
             }
 
-            AdjustTax(player);
-            DrawMap(player);
-            StatePurchases(player);
-            CheckNewTitle(player);
+            //AdjustTax(player);
+            //DrawMap(player);
+            //StatePurchases(player);
+            //CheckNewTitle(player);
 
             player.Year++;
             if (player.Year == player.YearOfDeath)
             {
-                ImDead(player);
+                //ImDead(player);
             }
 
             if (player.TitleNum >= 7)
             {
                 player.IWon = true;
+            }
+        }
+
+        private int ReleaseGrain(Player player)
+        {
+            double xp, zp;
+            double x, z;
+            string result;
+            int howMuch, maximum, minimum;
+            bool isOK;
+            isOK = false;
+            howMuch = 0;
+            minimum = player.GrainReserve / 5;
+            maximum = player.GrainReserve - minimum;
+
+            while(isOK is false)
+            {
+                Console.WriteLine("How much grain will you release for consumption?");
+                Console.WriteLine("1 = Minimum ({0})", minimum);
+                Console.WriteLine("2 = Maximum ({1})", maximum);
+                Console.Write("or enter a number: ");
+
+                result = Console.ReadLine() ?? "0";
+
+                howMuch = Convert.ToInt32(result);
+
+                if (howMuch == 1)
+                    howMuch = minimum;
+
+                if (howMuch == 2)
+                    howMuch = maximum;
+
+                //Are we being Scrooge?
+                if (howMuch < minimum)
+                    Console.WriteLine("You must at least release 20% of your reserves.");
+                else if (howMuch > maximum)
+                    //Whoa! Slow down there son!
+                    Console.WriteLine("You must keep at least 20%!");
+                else
+                    isOK = true;
+            }
+
+            player.SoldierPay = 0;
+            player.MarketRevenue = 0;
+            player.NewSerfs = 0;
+            player.DeadSerfs = 0;
+            player.TransplantedSerfs = 0;
+            player.FleeingSerfs = 0;
+            player.InvadeMe = false;
+            player.GrainReserve -= howMuch;
+
+            z = Convert.ToDouble(howMuch) / Convert.ToDouble(player.GrainDemand) - 1.0;
+
+            if (z > 0.0)
+            {
+                z /= 2.0;
+            }
+
+            if (z > 0.25)
+            {
+                z = z / 10.0 + 0.25;
+            }
+
+            zp = 50.0 - Convert.ToDouble(player.CustomsDuty) - Convert.ToDouble(player.SalesTax) - Convert.ToDouble(player.IncomeTax);
+
+            if (zp < 0.0)
+            {
+                zp *= Convert.ToDouble(player.Justice);
+            }
+
+            zp /= 10.0;
+
+            if (zp > 0.0)
+            {
+                zp += (3.0 - Convert.ToDouble(player.Justice));
+            }
+
+            z += Convert.ToDouble(zp) / 10.0;
+
+            if (z > 0.5)
+                z = 0.5;
+
+            if (howMuch < (player.GrainDemand - 1))
+            {
+                x = (Convert.ToDouble(player.GrainDemand) - Convert.ToDouble(howMuch)) / player.GrainDemand * 100.0 - 9.0;
+
+                xp = Convert.ToDouble(x);
+
+                if (x > 65.0)
+                    x = 65.0;
+
+                if (x < 0.0)
+                {
+                    xp = 0.0;
+                    x = 0.0;
+                }
+
+                SerfsProcreating(player, 3.0);
+                SerfsDecomposing(player, xp + 8.0);
+            }
+            else
+            {
+                SerfsProcreating(player, 7.0);
+                SerfsDecomposing(player, 3.0);
+
+                if ((player.CustomsDuty + player.SalesTax) < 35)
+                    player.Merchants += Random(4);
+
+                if (player.IncomeTax < Random(28))
+                {
+                    player.Nobles += Random(2);
+                    player.Clergy += Random(3);
+                }
+
+                if (howMuch > Convert.ToInt32(Convert.ToDouble(player.GrainDemand) * 1.3))
+                {
+                    zp = Convert.ToDouble(player.Serfs) / 1000.0;
+                    z = Convert.ToDouble(howMuch - player.GrainDemand) / Convert.ToDouble(player.GrainDemand) * 10.0;
+                    z *= (zp * Convert.ToDouble(Random(25)));
+                    z += Convert.ToDouble(Random(40));
+                    player.TransplantedSerfs = Convert.ToInt32(z);
+                    player.Serfs += player.TransplantedSerfs;
+                    Console.WriteLine("{0} serfs move to the city.", player.TransplantedSerfs);
+                    zp = Convert.ToDouble(z);
+                    z = (Convert.ToDouble(zp) * Convert.ToDouble(Randomizer.Next(0, Int16.MaxValue)) / Convert.ToDouble(Int16.MaxValue)); //((float)zp * (float)rand()) / (float)RAND_MAX;
+                    if (z > 50.0)
+                        z = 50.0;
+                    player.Merchants += Convert.ToInt32(z);
+                    player.Nobles++;
+                    player.Clergy += 2;
+                }
+            }
+
+            if (player.Justice > 2)
+            {
+                player.JusticeRevenue = player.Serfs / 100 * (player.Justice - 2) * (player.Justice - 2);
+                player.JusticeRevenue = Random(player.JusticeRevenue);
+
+                player.Serfs -= player.JusticeRevenue;
+                player.FleeingSerfs = player.JusticeRevenue;
+
+                Console.WriteLine("{0} serfs flee harsh justice.", player.FleeingSerfs);
+            }
+
+            player.MarketRevenue = player.Marketplaces * 75;
+
+            if (player.MarketRevenue > 0)
+            {
+                player.Treasury += player.MarketRevenue;
+                Console.WriteLine("Your market earned {0} florines.", player.MarketRevenue);
+            }
+
+            player.MillRevenue = player.Mills * (55 + Random(250));
+
+            if (player.MillRevenue > 0)
+            {
+                player.Treasury += player.MillRevenue;
+                Console.WriteLine("Your woolen mill earned {0} florins.", player.MillRevenue);
+            }
+
+            player.SoldierPay = player.Soldiers * 3;
+            player.Treasury -= player.SoldierPay;
+
+            Console.WriteLine("You paid your soldiers {0} florins.", player.SoldierPay);
+            Console.WriteLine("You have {0} serfs in your city.", player.Serfs);
+            Console.WriteLine("(Press Enter)");
+
+            _ = Console.ReadLine();
+
+            if ((player.Land / 1000) > player.Soldiers)
+            {
+                player.InvadeMe = true;
+                return 3;
+            }
+
+            if ((player.Land / 500) > player.Soldiers)
+            {
+                player.InvadeMe = true;
+                return 3;
+            }
+
+            return 0;
+
+        }
+
+        private void SerfsProcreating(Player player, double myScale)
+        {
+            int absc = 0;
+            double ord = 0.0;
+
+            absc = Convert.ToInt32(myScale);
+            ord = myScale - Convert.ToDouble(absc);
+
+            player.NewSerfs = Convert.ToInt32((Random(absc) + ord) * Convert.ToDouble(player.Serfs) / 100.0);
+            player.Serfs += player.NewSerfs;
+
+            Console.WriteLine("{0} serfs born this year.", player.Serfs);
+        }
+
+        private void SerfsDecomposing(Player player, double myScale)
+        {
+            int absc = 0;
+            double ord = 0.0;
+
+            absc = Convert.ToInt32(myScale);
+            ord = myScale - Convert.ToDouble(absc);
+
+            player.DeadSerfs = Convert.ToInt32((Random(absc) + ord) * Convert.ToDouble(player.Serfs) / 100.0);
+            player.Serfs += player.DeadSerfs;
+
+            Console.WriteLine("{0} serfs born this year.", player.Serfs);
+        }
+
+        private void BuySellGrain(Player player)
+        {
+            bool finished = false;
+            string result = string.Empty;
+
+            while(finished is false)
+            {
+                Console.WriteLine("Year {0:d}", player.Year);
+                Console.WriteLine();
+                Console.WriteLine("{0} {1}", player.Title, player.Name);
+                Console.WriteLine("Rats ate {0:d} of your grain reserves.", player.Rats);
+                PrintGrain(player);
+                Console.WriteLine("Grain\tGrain\tPrice of\tPrice of\tTreasury");
+                Console.WriteLine("Reserve\tDemand\tGrain\t\tLand");
+                Console.WriteLine("{0:d}\t{1:d}\t{2:d}\t\t{3}\t\t{4}", player.GrainReserve, player.GrainDemand, player.GrainPrice, player.LandPrice, player.Treasury);
+                Console.WriteLine("steres\tsteres\t1000 st.\thectare\t\tgold florins");
+                Console.WriteLine();
+                Console.WriteLine("You have {0} hectares of land.", player.Land);
+                Console.WriteLine();
+                Console.WriteLine("1. Buy Grain");
+                Console.WriteLine("2. Sell Grain");
+                Console.WriteLine("3. Buy land");
+                Console.WriteLine("4. Sell land");
+                Console.Write("(Enter q to continue): ");
+
+                result = Console.ReadLine()?.ToLower() ?? "q";
+                if (result[0] == 'q')
+                    finished = true;
+
+                if (result[0] == '1')
+                    BuyGrain(player);
+
+                if (result[0] == '2')
+                    SellGrain(player);
+
+                if (result[0] == '3')
+                    BuyLand(player);
+
+                if (result[0] == '4')
+                    SellLand(player);
+            }
+
+        }
+        private void BuyGrain(Player player)
+        {
+            int howMuch;
+            string result;
+
+            Console.Write("How much grain do you want to buy (0 to specify a total)? ");
+
+            result = Console.ReadLine() ?? "0";
+
+            howMuch = Convert.ToInt32(result);
+
+            if (howMuch == 0)
+            {
+                Console.Write("How much total grain do you wish? ");
+                result = Console.ReadLine() ?? "0";
+                howMuch = Convert.ToInt32(result);
+                howMuch -= player.GrainReserve;
+                if (howMuch < 0)
+                {
+                    Console.WriteLine("Invalid total amount.");
+                    Console.WriteLine();
+                    return;
+                }
+            }
+
+            player.Treasury -= (howMuch * player.GrainPrice / 1000);
+            player.GrainReserve += howMuch;
+        }
+
+        private void SellGrain(Player player)
+        {
+            int howMuch;
+            string result;
+
+            Console.Write("How much grain do you want to sell? ");
+
+            result = Console.ReadLine() ?? "0";
+
+            howMuch = Convert.ToInt32(result);
+
+            if (howMuch > player.GrainReserve)
+            {
+                Console.WriteLine("You don't have it.");
+                return;
+            }
+
+            player.Treasury += (howMuch * player.GrainPrice / 1000);
+            player.GrainReserve -= howMuch;
+        }
+        private void BuyLand(Player player)
+        {
+            int howMuch;
+            string result;
+
+            Console.Write("How much land do you want to buy? ");
+
+            result = Console.ReadLine() ?? "0";
+
+            howMuch = Convert.ToInt32(result);
+
+            player.Treasury -= Convert.ToInt32(howMuch * player.LandPrice);
+            player.Land += howMuch;
+        }
+
+        private void SellLand(Player player)
+        {
+            int howMuch;
+            string result;
+
+            Console.Write("How much land do you want to sell? ");
+
+            result = Console.ReadLine() ?? "0";
+
+            howMuch = Convert.ToInt32(result);
+
+            if (howMuch > (player.Land - 5000))
+            {
+                Console.WriteLine("You can't sell that much.");
+                return;
+            }
+
+            player.Treasury += Convert.ToInt32(howMuch * player.LandPrice);
+            player.Land -= howMuch;
+        }
+
+        private void PrintGrain(Player player)
+        {
+            switch (player.Harvest)
+            {
+                case 0:
+                case 1:
+                    Console.WriteLine("Drought. Famine Threatens. ");
+                    break;
+                case 2:
+                    Console.WriteLine("Bad Weather. Poor Harvest. ");
+                    break;
+                case 3:
+                    Console.WriteLine("Normal Weather. Average Harvest. ");
+                    break;
+                case 4:
+                    Console.WriteLine("Good Weather. Fine Harvest. ");
+                    break;
+                case 5:
+                    Console.WriteLine("Excellent Weather. Great Harvest! ");
+                    break;
             }
         }
 

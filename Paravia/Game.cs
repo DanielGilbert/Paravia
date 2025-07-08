d43c91c16ebe940e541626aa99d7ac54a1b8020c
Daniel Gilbert <ich@danielgilbert.de>
2025-07-08 16:19:47 +0200

    Uses Top-Level-Statements

---
+++ Paravia/Game.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Paravia
+{
+    public class Game
+    {
+        public void Run()
+        {
+
+        }
+    }
+}
